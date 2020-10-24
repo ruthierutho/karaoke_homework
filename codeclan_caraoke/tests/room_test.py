5fedbe195117b609c1468746354207f7233ad345
Ruth Foulis <ruthlilfoulis@gmail.com>
2020-10-24 12:28:29 +0100

    room and room tests

--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -1,13 +1,27 @@
 import unittest
 from classes.room import *
+from classes.song import *
 
 
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room1 = Room("Britney Room", 12, [["I'm a Slave 4 u", "Britney Spears"], ["Toxic", "Britney Spears"], ["...Baby One More Time", "Britney Spears"]])
-        self.room2 = Room("Christina Room", 6, [["Dirrty", "Christina Aguilera"], ["Genie in a Bottle", "Christina Aguilera"], ["Beautiful", "Christina Aguilera"]])
-        self.room3 = Room("Madonna Room", 10, [["Like a Prayer", "Madonna"], ["Hung Up", "Madonna"], ["Material Girl", "Madonna"]])
+        self.song1 = Song("I'm a Slave 4 u", "Britney Spears")
+        self.song2 = Song("Toxic", "Britney Spears")
+        self.song3 = Song("...Baby One More Time", "Britney Spears")
+        self.song4 = Song("Dirrty", "Christina Aguilera")
+        self.song5 = Song("Genie in a Bottle", "Christina Aguilera")
+        self.song6 = Song("Beautiful", "Christina Aguilera")
+        self.song7 = Song("Like a Prayer", "Madonna")
+        self.song8 = Song("Hung Up", "Madonna")
+        self.song9 = Song("Material Girl", "Madonna")
+
+        self.room1 = Room("Britney Room", 12, [self.song1, self.song2, self.song3])
+        self.room2 = Room("Christina Room", 6, [self.song4, self.song5, self.song6])
+        self.room3 = Room("Madonna Room", 10, [self.song7, self.song8, self.song9])
 
     def test_room_has_name(self):
         self.assertEqual("Britney Room", self.room1.name)
+
+    def test_room_capacity(self):
+        self.assertEqual(10, self.room3.capacity)
